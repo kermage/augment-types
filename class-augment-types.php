09ed91524109f0bede3a3a3e900b37fdb0f306b7
kermage <genealyson.torcende@gmail.com>
2018-11-06 08:59:53 +0800

    create sorting page for each registered type

--- class-augment-types.php
+++ class-augment-types.php
@@ -24,6 +24,57 @@ class Augment_Types {
 
 	private function __construct() {
 
+		add_action( 'admin_menu', array( $this, 'menu' ) );
+
+	}
+
+
+	public function menu() {
+
+		$types  = get_post_types();
+
+		foreach( $types as $type ) {
+
+			$params['id']     = 'at-sort_' . $type;
+			$params['parent'] = 'edit.php?post_type=' . $type;
+
+			$this->page( $params );
+
+		}
+
+	}
+
+
+	private function page( $params ) {
+
+		add_submenu_page(
+			// Parent Slug
+			$params['parent'],
+			// Page Title
+			__( 'Sort Types', 'augment-types' ),
+			// Menu Title
+			__( 'Sort Types', 'augment-types' ),
+			// Capability
+			'manage_options',
+			// Menu Slug
+			$params['id'],
+			// Content Function
+			array( $this, 'create' )
+		);
+
+	}
+
+
+	public function create() {
+
+		?>
+
+		<div class="wrap">
+			<h1><?php echo get_admin_page_title(); ?></h1>
+		</div>
+
+		<?php
+
 	}
 
 }
