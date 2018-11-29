0996acbaffd86bc52ce212534882e8617f176a77
kermage <genealyson.torcende@gmail.com>
2018-11-29 22:50:18 +0800

    Bump version to 0.4.0

--- augment-types.php
+++ augment-types.php
@@ -11,7 +11,7 @@
  * Author:      Gene Alyson Fortunado Torcende
  * Author URI:  mailto:genealyson.torcende@gmail.com
  * Description: Add essential functionalities to WordPress Post Types.
- * Version:     0.3.0
+ * Version:     0.4.0
  * License:     GNU General Public License v3.0
  * License URI: https://www.gnu.org/licenses/gpl-3.0.html
  */
@@ -26,7 +26,7 @@ Global constants
 ================================================== */
 
 if ( ! defined( 'AT_VERSION' ) ) {
-	define( 'AT_VERSION', '0.3.0' );
+	define( 'AT_VERSION', '0.4.0' );
 }
 
 if ( ! defined( 'AT_FILE' ) ) {
