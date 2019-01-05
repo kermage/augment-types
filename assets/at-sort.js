74a7d49c3cf94c2a39c45512985fb2bc2a681881
kermage <genealyson.torcende@gmail.com>
2019-01-05 13:45:05 +0800

    update the column order numbers after sort

--- assets/at-sort.js
+++ assets/at-sort.js
@@ -8,6 +8,15 @@
 	var $filters   = $( '#the-filters' );
 
 
+	function update_order_number() {
+		var $items = $container.find( 'tr td:first-child' );
+
+		$items.each( function( $index ) {
+			$( this ).html( $items.length - $index - 1 );
+		});
+	}
+
+
 	function at_order_callback() {
 		$.ajax( {
 			type : 'POST',
@@ -23,6 +32,8 @@
 					.addClass( 'sorting' );
 			},
 			complete: function() {
+				update_order_number();
+
 				$container
 					.sortable( 'enable' )
 					.parents( '.wp-list-table' )
